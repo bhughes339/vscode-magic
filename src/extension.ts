b41ca53f80193ec509dfb4bfc027571d1355d712
bhughes339 <bhughes339@gmail.com>
2018-03-08 12:37:31 -0500

    push activation events into ExtensionContext

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,7 @@ import MagicHoverProvider from './features/hoverProvider';
 import { MagicDocFormatProvider, MagicSelFormatProvider } from './features/formatProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "magic" is now active!');
-    vscode.languages.registerDocumentFormattingEditProvider('magic', new MagicDocFormatProvider);
-    vscode.languages.registerDocumentRangeFormattingEditProvider('magic', new MagicSelFormatProvider);
-    vscode.languages.registerHoverProvider('magic', new MagicHoverProvider);
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('magic', new MagicDocFormatProvider));
+    context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider('magic', new MagicSelFormatProvider));
+    context.subscriptions.push(vscode.languages.registerHoverProvider('magic', new MagicHoverProvider));
 }
