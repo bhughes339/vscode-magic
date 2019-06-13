351cbfee0bc77bc8e51b16a2e2eaf002e20639ff
bhughes339 <bhughes339@gmail.com>
2019-06-13 09:19:54 -0400

    add hover support for MRI visit nodes

--- src/features/hoverProvider.ts
+++ src/features/hoverProvider.ts
@@ -7,15 +7,24 @@ export default class MagicHoverProvider implements vscode.HoverProvider {
     public provideHover(document: TextDocument, position: Position): ProviderResult<Hover | undefined> {
         let config = vscode.workspace.getConfiguration("magic");
         let hoverSeconds = config.get("hoverSeconds");
+        let hoverVisitNodes = config.get("hoverVisitNodes");
         let range = document.getWordRangeAtPosition(position,
             new RegExp('/?' + Util.constants.wordRange.source));
         let word = (range) ? document.getText(range) : null;
         if (word) {
+            let match;
+            if ((match = word.match(/^(\d{8})\.(\d{4})$/)) && hoverVisitNodes) {
+                let fulldate = (99999999 - parseInt(match[1])).toString();
+                let time = (9999 - parseInt(match[2])).toString().padStart(4, '0');
+                let date = new Date(parseInt(fulldate.slice(0, 4)), parseInt(fulldate.slice(4, 6)) - 1, parseInt(fulldate.slice(6)),
+                    parseInt(time.slice(0, 2)), parseInt(time.slice(2)));
+                return new Hover(date.toLocaleString() + ` (${fulldate} ${time})`);
+            }
             if (word.match(/^[\d]+$/) && hoverSeconds) {
                 let epochSeconds = parseInt(word) + Util.constants.epochDelta;
                 if (epochSeconds < 2147483647) {
                     let date = new Date(epochSeconds * 1000);
-                    return new Hover('S(0) Date: ' + date.toLocaleString())
+                    return new Hover('S(0) Date: ' + date.toLocaleString());
                 }
             } else {
                 word = word.replace(/[.]/g, '\\$&');
